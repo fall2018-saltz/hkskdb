3c1746a36ab8569bb948675a17b5cfabef69460d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-25 03:39:51 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,13 +1,2 @@
 
 raw_data
-rename=colnames(raw_data)
-
-rename[1]=("statename")
-rename[c(2,3,4)]=c("gdg","vgdjh","yfgdyu")
-rename
-colnames(raw_data)=rename
-raw_data
-which.max(raw_data$yfgdyu)
-raw_data[53,]
-popsort=raw_data[-order(raw_data$yfgdyu),]
-popsort
