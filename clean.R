ea9fcb588e1596449e5d89495748bf00597c42d0
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-24 21:54:00 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -4,18 +4,3 @@ rename=colnames(raw_data)
 rename[1]=("statename")
 rename[c(2,3,4)]=c("gdg","vgdjh","yfgdyu")
 rename
-
-cleandata=function(dataframe){
-    
-    
-    name=colnames(df)
-    name[c(1,2,3,4)]=c("reddy","srinivas","pachika","syracuse")
-    df=name
-    colnames(df)=name
-    return(df)
-    
-}
-raw_data
-dfstates=cleandata(raw_data)
-dfstates
-raw_data
