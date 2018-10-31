473fa5403354bae315e721422eac4fe3838f9991
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 02:00:26 -0400

    Saving script2435.R

--- script2435.R
+++ script2435.R
@@ -1,1 +1,11 @@
 
+#copying USArrests into a local variable
+arrestsinus<- USArrests
+
+#creating a column name for rows having states 
+rownames(clean) <- as.vector(clean[,"stateName"])
+
+#merging 2 dataframes
+mergeData <- merge(clean, arrestsinus, by = "row.names")
+mergeData
+
