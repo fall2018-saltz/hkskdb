500162847bd86e0b77f47b9e6ab44b7da75ec9a2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 02:07:20 -0400

    Saving script7294.R

--- script7294.R
+++ script7294.R
@@ -1,9 +1,12 @@
 
+#Step B: Generate a color coded map
+#3)	Create a color coded map, based on the area of the state 
+
 library("ggplot2")
 library("ggmap")
-us <- map_data("state")
+usmap <- map_data("state")
 options(scipen=999)
-map2 <- ggplot(clean_data_merged, aes(map_id = stateName))
-map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")             
-map2 <- map2 + expand_limits(x = us$long, y = us$lat) + coord_map()
+map2 <- ggplot(mergeddata, aes(map_id = stateName))
+map2 <- map2 + geom_map(map = usmap,aes(fill=Murder))+ scale_fill_gradient(low = "green", high = "red")             
+map2 <- map2 + expand_limits(x = usmap$long, y = usmap$lat) + coord_map()
 map2
