bbb94aefc5a0273ef6adffc3da543d405aa82e48
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 02:08:35 -0400

    Saving script7294.R

--- script7294.R
+++ script7294.R
@@ -6,7 +6,7 @@ library("ggplot2")
 library("ggmap")
 us <- map_data("state")
 options(scipen=999)
-map2 <- ggplot(clean_data_merged, aes(map_id = stateName))
+map2 <- ggplot(mergeddata, aes(map_id = stateName))
 map2 <- map2 + geom_map(map = us,aes(fill=Murder))+ scale_fill_gradient(low = "black", high = "red")             
 map2 <- map2 + expand_limits(x = us$long, y = us$lat) + coord_map()
 map2
