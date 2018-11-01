205566ff58506dd2f44e1d2764e3014b89cd15bc
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 00:38:17 -0400

    Saving script2435.R

--- script2435.R
+++ script2435.R
@@ -1,11 +1,26 @@
 
-#copying USArrests into a local variable
-arrestsinus<- USArrests
+library("ggplot2")
+library("ggmap")
+cleandata2 <- USArrests
 
-#creating a column name for rows having states 
-rownames(clean) <- as.vector(clean[,"stateName"])
 
-#merging 2 dataframes
-mergeData <- merge(clean, arrestsinus, by = "row.names")
-mergeData
+# save row names as a separate variable
+stateName <- as.vector(rownames(USArrests))
+stateName <- tolower(stateName)
+# add this column to the dataframe as a column names stateName
+cleandata2 <- cbind(cleandata2,stateName, stringsAsFactors=FALSE)
 
+# merge the dataframes based on the stateName column in both the dataframes
+# the merge function combines dataframes based on common columns
+# here it merges the dataframes by the common column stateName
+
+clean_data_merged <- merge(clean_data,cleandata2,by="stateName")
+
+#clean_data_merged
+statecenterx <- state.center$x
+statecentery <- state.center$y
+clean_data_merged <- cbind(clean_data_merged,state.area)
+clean_data_merged <- cbind(clean_data_merged,statecenterx)
+clean_data_merged <- cbind(clean_data_merged,statecentery)
+
+str(clean_data_merged)
