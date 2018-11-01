37e2e2328215c5e4cde6a37269d1ac50ea36f75f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 02:02:04 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -2,19 +2,15 @@
 #1)	Read in the census dataset and the USArrests and merge them 
 # cleaning data
 newfun<- function(states){
-
-  # removes the first row and the last row
-  # and the first four columns SUMLEV	REGION	DIVISION	STATE
-
-  dfSt <- states[,c(-1,-2,-3,-4)]
-  dfSt <- dfSt[c(-1,-10,-53),] 
+ dfSt <- states[,c(-1,-2,-3,-4)]
+ dfSt <- dfSt[c(-1,-10,-53),] 
 
   # renaming the remaining columns
   #using colnames function we are renaming columns 
   # we are using row names function to rename rows
   colnames(dfSt)[c(1,2,3,4)] <- c("stateName", "population", "popOver18", "percentOver18")
 
-  #returns the clean dataframe
+
   return(dfSt)
 }
 
