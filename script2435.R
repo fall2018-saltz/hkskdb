eb554ba00c82de7a03b254a045d929116f85201d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 00:40:27 -0400

    Saving script2435.R

--- script2435.R
+++ script2435.R
@@ -9,7 +9,7 @@ sta_name <- as.vector(rownames(USArrests))
 
 sta_name  <- tolower(sta_name )
 # add this column to the dataframe as a column names stateName
-cleandata2 <- cbind(cleandata2,stateName, stringsAsFactors=FALSE)
+clean <- cbind(clean,stateName, stringsAsFactors=FALSE)
 
 # merge the dataframes based on the stateName column in both the dataframes
 # the merge function combines dataframes based on common columns
