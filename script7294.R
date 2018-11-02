d8b20556c673496cb948962f1480c521a10bd253
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 22:25:05 -0400

    Saving script7294.R

--- script7294.R
+++ script7294.R
@@ -7,16 +7,19 @@ stateName <- as.vector(rownames(data2))
 
 stateName <- tolower(stateName)
 
-arrests=cbind(data2,stateName,stringsAsFactors=FALSE)
+arrests <- cbind(data2,stateName,stringsAsFactors=FALSE)
 
 #meging both data1 and data2 dataframes
 
 mergedData=merge(data1,data2,by="stateName")
 mergedData
 
-#2)	Add the area of each state, and the center of each state, to the merged dataframe, using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
-stCenterX <- state.center$x
-stCenterY <- state.center$y
+#2)	Add the area of each state, and the center of each state, to the merged dataframe,
+#using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
+ 
+ 
+stX <- state.center$x
+stY <- state.center$y
 mergedData <- cbind(mergedData,state.area)
-mergedData <- cbind(mergedData,stCenterX)
-mergedData <- cbind(mergedData,stCenterY)
+mergedData <- cbind(mergedData,stX )
+mergedData <- cbind(mergedData,stY)
