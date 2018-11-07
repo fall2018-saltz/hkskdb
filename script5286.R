9df6a56bf77a69e761044f36449bca38ad9b501f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-07 01:09:07 -0500

    Saving script5286.R

--- script5286.R
+++ script5286.R
@@ -1,8 +1,10 @@
 
-#take the USArrests data set into arrests variable 
+#passing USArrests into arrests variable
+
 arrests <- USArrests
 
 stateName <- as.vector(rownames(arrests))
+
 stateName <- tolower(stateName)
 
 arrests <- cbind(arrests,stateName,stringsAsFactors=FALSE)
@@ -12,5 +14,6 @@ arrests <- cbind(arrests,stateName,stringsAsFactors=FALSE)
 # creating  a column to store state names and meging two data frames "data1" and "arrests"
 
 mergeDF=merge(data1 ,arrests,by="stateName")
+
 mergeDF
 
