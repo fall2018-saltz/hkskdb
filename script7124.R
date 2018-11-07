624fdf054522c6fe6086b59dac8beda7bbd1ea66
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-07 01:16:25 -0500

    Saving script7124.R

--- script7124.R
+++ script7124.R
@@ -62,13 +62,20 @@ Map2
 
 #5)	 showing population as circle . Larger the population Larger the size of circle
 
+
 Map3 <- ggplot(mergeDF, aes(map_id = stateName))
+
 Map3 <- Map3+geom_map(map = usa,aes(fill=Murder))+ scale_fill_gradient(low = "yellow", high = "blue")             
+
 Map3 <- Map3+expand_limits(x = usa$long, y = usa$lat) + coord_map()
 
-#for larger the population, larger is the circle
+
+
+
 Map3 <- Map3+geom_point(mergeDF, mapping = aes(x = CenterX, y=CenterY, size=population))
+
 Map3 <- Map3+ggtitle("Map Of Murder population ")
+
 Map3
 
 #Step D: Zoom the map
@@ -86,6 +93,7 @@ y2 <- latitude$lat+10
 
 #plot the map for the above data
 Map4 <- ggplot(mergeDF, aes(map_id=stateName))
+
 #based on murder rate
 Map4 <- Map4 + geom_map(map=usa,aes(fill=Murder))+ scale_fill_gradient(low = "yellow", high = "blue") + xlim(c(x1,x2)) + ylim(c(y1,y2))        
 Map4 <- Map4+ expand_limits(x=usa$long, y = usa$lat) + coord_map()
