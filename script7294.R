f24814fe77ae2f871ef4ecf4ac8c6d1678046181
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-06 21:47:39 -0500

    Saving script7294.R

--- script7294.R
+++ script7294.R
@@ -2,12 +2,7 @@
 USArrests
 
 
-states <- states[ , -1:-4]    # Removing first 4 columns
-colnames(states) <- c("stateName", "population", "popOver18", "percentOver18") # Changing column names
-View(states)
 
-arrests$stateName <- rownames(arrests)
-arrests$stateName
 
 mergeDF <- merge(states, arrests, by = "stateName") #merging the dataframes
 View(mergeDF)
