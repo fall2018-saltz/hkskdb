61f7d54a896e79e8738dcdb37708aa02157ead79
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-06 22:06:55 -0500

    Saving script5286.R

--- script5286.R
+++ script5286.R
@@ -1,9 +1,17 @@
 
-USArrests
-arres <- USArrests
+#take the USArrests data set into arrests variable 
+arrests=USArrests
+
+stateName=as.vector(rownames(arrests))
+stateName=tolower(stateName)
+
+arrests=cbind(arrests,stateName,stringsAsFactors=FALSE)
+
+#arrests doesn't contain a proper column name for state names but has state names as index
+#so create a column to store the state names suing rownames
 
 
-mergeDF <- merge(data1, arres, by = "stateName") #merging the dataframes
-
-str(mergeDF)
-summary(mergeDF)
+#now that we have to merge two dataframes using a common element 
+#the column element is stateName
+mergedData=merge(cleandata,arrests,by="stateName")
+mergedData
