8f459641e193ac73b175d6c3c70e9f3ebac48423
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-06 21:43:52 -0500

    Saving script7294.R

--- script7294.R
+++ script7294.R
@@ -1,26 +1,15 @@
 
-#copying usarests into variable called data2
-data2<-USArrests
+arrests <- USArrests
+View(arrests)
 
+states <- states[ , -1:-4]    # Removing first 4 columns
+colnames(states) <- c("stateName", "population", "popOver18", "percentOver18") # Changing column names
+View(states)
 
-stateName <- as.vector(rownames(data2))
+arrests$stateName <- rownames(arrests)
+arrests$stateName
 
-stateName <- tolower(stateName)
-data2
-
-arrests <- cbind(data2,stateName,stringsAsFactors=FALSE)
-
-#meging both data1 and data2 dataframes
-
-mergedData <- rbind(data1,data2)
-mergedData
-
-#2)	Add the area of each state, and the center of each state, to the merged dataframe,
-#using the ‘state.center’, ‘state.center’ and ‘state.name’ vectors
- 
- 
-stX <- state.center$x
-stY <- state.center$y
-mergedData <- cbind(mergedData,state.area)
-mergedData <- cbind(mergedData,stX )
-mergedData <- cbind(mergedData,stY)
+mergeDF <- merge(states, arrests, by = "stateName") #merging the dataframes
+View(mergeDF)
+str(mergeDF)
+summary(mergeDF)
